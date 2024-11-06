0728d1d62b9f64432d084fa9146945387a40de5c
Gabrieel-Santos <maginovam@gmail.com>
2024-11-06 09:34:27 -0300

    agora vai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import Footer from "./components/Footer";
@@ -15,8 +10,6 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/add-task" element={<AddTask />} />
-        {/* Redireciona qualquer rota inválida para "/tasks" */}
-        <Route path="*" element={<Navigate to="/tasks" />} />
       </Routes>
       <Footer />
     </Router>
