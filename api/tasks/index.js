fb3d31a87776410acecbd6f2e18e45f4baab600a
Gabrieel-Santos <maginovam@gmail.com>
2024-11-06 16:19:44 -0300

    dass

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://gerenciamento-tarefas.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
