65364fc56bb7f22e76a6b09c0a7a4760359de218
Gabrieel-Santos <maginovam@gmail.com>
2024-11-06 11:24:41 -0300

    boa

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -2,16 +2,12 @@ import express from "express";
 import cors from "cors";
 import taskRoutes from "./taskRoutes.js";
 
-// Criação da aplicação Express
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "https://gerenciamento-tarefas.vercel.app" }));
 
 // Configuração das rotas para tarefas
-app.use("/api/tasks", taskRoutes); // Ajuste o caminho para o endpoint desejado
+app.use("/api/tasks", taskRoutes);
 
-// Exportação da função para o ambiente serverless
-export default async (req, res) => {
-  // Passa a requisição e a resposta para o app Express
-  await app(req, res);
-};
+// Exportação do `app`
+export default app;
