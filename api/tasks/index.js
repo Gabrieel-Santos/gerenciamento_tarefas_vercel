cad58c40912f2796f35eaf10ef42c59035ddbd73
Gabrieel-Santos <maginovam@gmail.com>
2024-11-06 11:33:13 -0300

    vai

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -4,7 +4,12 @@ import taskRoutes from "./taskRoutes.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://gerenciamento-tarefas.vercel.app" }));
+app.use(
+  cors({
+    origin: "https://gerenciamento-tarefas.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // Configuração das rotas para tarefas
 app.use("/api/tasks", taskRoutes);
